af1a8568c450f24ccb96451f8a89275a84ba52ef
Ludo <97764715+ludo53@users.noreply.github.com>
2025-08-03 00:50:24 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/blockexplorer/main.go
@@ -0,0 +1,23 @@
+// cmd/blockexplorer/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"blockexplorer/internal/blockexplorer"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := blockexplorer.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
